a494cfa874e46406909cbef24158577030ce3b19
Anurag-XD <anurag@unicohub.com>
2024-07-08 10:17:56 +0530

    svg imports test error fix

--- jest.config.cjs
+++ jest.config.cjs
@@ -9,8 +9,8 @@ module.exports = {
   rootDir:"src",
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__tests__/__ mocks __/fileMock.js",
-    "\\.(css|less)$": " <rootDir>/__tests__/__ mocks __/styleMock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__tests__/__ mocks __/fileMock.js",
+    "/\.(css|less)$/": " <rootDir>/__tests__/__ mocks __/styleMock.js"
   },
   resolver: undefined,
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
